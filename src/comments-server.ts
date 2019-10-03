5a9c2ed9d3b47afe4ce1dda492b4ad5bdf864a74
Ruben Oostinga <ruben.oostinga@gmail.com>
2019-10-03 16:01:48 +0200

    Get transform-federation example working

---
+++ src/comments-server.ts
@@ -0,0 +1,45 @@
+import { ApolloServer, gql, ServerInfo } from 'apollo-server';
+import { buildFederatedSchema } from '@apollo/federation';
+
+const typeDefs = gql`
+  type Product @key(fields: "id") {
+    id: String!
+    price: Int
+    weight: Int
+  }
+
+  type Query {
+    findProduct: Product!
+  }
+`;
+
+interface ProductKey {
+  id: String;
+}
+
+const product = {
+  id: '123',
+  price: 899,
+  weight: 100,
+};
+
+const resolvers = {
+  Query: {
+    findProduct() {
+      return product;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  schema: buildFederatedSchema([
+    {
+      typeDefs,
+      resolvers,
+    },
+  ]),
+});
+
+server.listen({ port: 4002 }).then(({ url }: ServerInfo) => {
+  console.log(`🚀 Federation server ready at ${url}`);
+});
